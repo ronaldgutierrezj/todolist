951d1a6f822206e79f9bb80a86daf87423f444ad
ronald <ronaldgutierrezj@gmail.com>
2021-10-10 16:26:50 -0500

    9c recfator 3

---
+++ controllers/todo.js
@@ -0,0 +1,102 @@
+////////////////////////////////////////
+// Import Dependencies
+////////////////////////////////////////
+const express = require("express")
+const Todo = require("../models/todo")
+
+/////////////////////////////////////////
+// Create Route
+/////////////////////////////////////////
+const router = express.Router()
+
+/////////////////////////////////////////
+// Routes
+/////////////////////////////////////////
+
+// seed route
+router.get("/seed", (req, res) => {
+
+    const startTodos = [
+          { name: "Do project 2", category: "School", comment:"create an app then deployed to Heroku", date_to_do:"10/12/2021", due_date:"10/16/2021", completed: false },
+          { name: "Get groceries", category: "House", comment:"Go to Whole Food to get groceries for the week", date_to_do:"10/12/2021", due_date:"", completed: true },
+          { name: "Look for hotel", category: "Vaction", comment:"Look for a place to stay for trip to Aruba 2022", date_to_do:"10/20/2021", due_date:"12/16/2021", completed: false },
+          { name: "Get tickets concert", category: "Entertaiment", comment:"Get ticket for 50 cents concert", date_to_do:"10/12/2021", due_date:"10/20/2021", completed: true },
+        ]
+    Todo.deleteMany({}, (err, data) => {
+      Todo.create(startTodos,(err, data) => {
+          
+          res.json(data);
+        }
+      );
+    });
+  });
+  
+  // Index Route
+  
+  router.get("/", (req, res) => {
+    Todo.find({}, (err, todos) => {
+      res.render("todos/index.ejs", { todos });
+    });
+  });
+  
+  // New Route
+  router.get("/new", (req, res) => {
+    res.render("todos/new.ejs")
+  });
+  
+  router.post("/", (req, res) => {
+    
+    req.body.completed = req.body.completed === "on" ? true : false
+   
+    Todo.create(req.body, (err, todo) => {
+        // redirect the user back to the main fruits page after fruit created
+        res.redirect("/todos")
+    })
+  });
+  
+  // Delete Route
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id
+    Todo.findByIdAndRemove(id, (err, todo) => {
+        // redirect user back to index page
+        res.redirect("/todos")
+    })
+  });
+  
+  // edit route
+  router.get("/:id/edit", (req, res) => {
+    
+    const id = req.params.id
+    Todo.findById(id, (err, todo) => {
+        res.render("todos/edit.ejs", {todo})
+    })
+  });
+  
+  //update route
+  router.put("/:id", (req, res) => {
+    const id = req.params.id
+    req.body.completed = req.body.completed === "on" ? true : false
+    Todo.findByIdAndUpdate(id, req.body, {new: true}, (err, todo) => {
+        res.redirect("/todos")
+    })
+  });
+  
+  
+  // Show Route
+  
+  router.get("/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id
+  
+    // find the particular Todo from the database
+    Todo.findById(id, (err, todo) => {
+        // render the template with the data from the database
+        res.render("todos/show.ejs", {todo})
+    })
+  })
+  
+
+//////////////////////////////////////////
+// Export the Router
+//////////////////////////////////////////
+module.exports = router
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
-const Todo = require("./models/todo")
+const TodoRouter = require("./controllers/todo")
 
 
 
@@ -14,21 +14,13 @@ const app = express();
 //Middleware
 //___________________
 
-app.use(morgan("tiny")) 
-app.use(methodOverride("_method")) 
-app.use(express.urlencoded({extended: true})) 
-app.use(express.static("public"))
+app.use(morgan("tiny")); 
+app.use(methodOverride("_method")); 
+app.use(express.urlencoded({extended: true})); 
+app.use(express.static("public"));
+app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
+app.use("/todos", TodoRouter)
 
-////////////////////////
-//use public folder for static assets
-app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 ////////////////////////////
 
 //___________________
@@ -38,88 +30,6 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 app.get('/' , (req, res) => {
   res.send('Hello');
 });
-
-// seed route
-app.get("/todos/seed", (req, res) => {
-
-  const startTodos = [
-        { name: "Do project 2", category: "School", comment:"create an app then deployed to Heroku", date_to_do:"10/12/2021", due_date:"10/16/2021", completed: false },
-        { name: "Get groceries", category: "House", comment:"Go to Whole Food to get groceries for the week", date_to_do:"10/12/2021", due_date:"", completed: true },
-        { name: "Look for hotel", category: "Vaction", comment:"Look for a place to stay for trip to Aruba 2022", date_to_do:"10/20/2021", due_date:"12/16/2021", completed: false },
-        { name: "Get tickets concert", category: "Entertaiment", comment:"Get ticket for 50 cents concert", date_to_do:"10/12/2021", due_date:"10/20/2021", completed: true },
-      ]
-  Todo.deleteMany({}, (err, data) => {
-    Todo.create(startTodos,(err, data) => {
-        
-        res.json(data);
-      }
-    );
-  });
-});
-
-// Index Route
-
-app.get("/todos", (req, res) => {
-  Todo.find({}, (err, todos) => {
-    res.render("todos/index.ejs", { todos });
-  });
-});
-
-// New Route
-app.get("/todos/new", (req, res) => {
-  res.render("todos/new.ejs")
-});
-
-app.post("/todos", (req, res) => {
-  
-  req.body.completed = req.body.completed === "on" ? true : false
- 
-  Todo.create(req.body, (err, todo) => {
-      // redirect the user back to the main fruits page after fruit created
-      res.redirect("/todos")
-  })
-});
-
-// Delete Route
-app.delete("/todos/:id", (req, res) => {
-  const id = req.params.id
-  Todo.findByIdAndRemove(id, (err, todo) => {
-      // redirect user back to index page
-      res.redirect("/todos")
-  })
-});
-
-// edit route
-app.get("/todos/:id/edit", (req, res) => {
-  
-  const id = req.params.id
-  Todo.findById(id, (err, todo) => {
-      res.render("todos/edit.ejs", {todo})
-  })
-});
-
-//update route
-app.put("/todos/:id", (req, res) => {
-  const id = req.params.id
-  req.body.completed = req.body.completed === "on" ? true : false
-  Todo.findByIdAndUpdate(id, req.body, {new: true}, (err, todo) => {
-      res.redirect("/todos")
-  })
-});
-
-
-// Show Route
-
-app.get("/todos/:id", (req, res) => {
-  // get the id from params
-  const id = req.params.id
-
-  // find the particular Todo from the database
-  Todo.findById(id, (err, todo) => {
-      // render the template with the data from the database
-      res.render("todos/show.ejs", {todo})
-  })
-})
 
 //___________________
 //Listener
