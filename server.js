7da3ca011f40552da0ecd5ec6e27d0a43d70cf14
ronald <ronaldgutierrezj@gmail.com>
2021-10-10 14:25:55 -0500

    3 commit index route added

--- server.js
+++ server.js
@@ -74,6 +74,32 @@ app.get('/' , (req, res) => {
   res.send('Hello');
 });
 
+// seed route
+app.get("/todos/seed", (req, res) => {
+
+  const startTodos = [
+        { name: "Do project 2", category: "School", comment:"create an app then deployed to Heroku", date_to_do:"10/12/2021", due_date:"10/16/2021", completed: false },
+        { name: "Get groceries", category: "House", comment:"Go to Whole Food to get groceries for the week", date_to_do:"10/12/2021", due_date:"", completed: true },
+        { name: "Look for hotel", category: "Vaction", comment:"Look for a place to stay for trip to Aruba 2022", date_to_do:"10/20/2021", due_date:"12/16/2021", completed: false },
+        { name: "Get tickets concert", category: "Entertaiment", comment:"Get ticket for 50 cents concert", date_to_do:"10/12/2021", due_date:"10/20/2021", completed: true },
+      ]
+  Todo.deleteMany({}, (err, data) => {
+    Todo.create(startTodos,(err, data) => {
+        
+        res.json(data);
+      }
+    );
+  });
+});
+
+// Iindex Route
+
+app.get("/todos", (req, res) => {
+  Todo.find({}, (err, todos) => {
+    res.render("todos/index.ejs", { todos });
+  });
+});
+
 //___________________
 //Listener
 //___________________
